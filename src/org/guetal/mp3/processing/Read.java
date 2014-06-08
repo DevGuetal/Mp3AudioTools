a7254a426c780e0e50635e1ba32350ffa892837c
DevGuetal <dev.guetal@gmail.com>
2014-06-08 11:28:58 +0100

    Cleanup of Read.java
    - removed useless code
    - log cleanup
    - file stream handling modified

--- src/org/guetal/mp3/processing/Read.java
+++ src/org/guetal/mp3/processing/Read.java
@@ -8,40 +8,49 @@
  */
 
 package org.guetal.mp3.processing;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.logging.Logger;
+
+import org.guetal.mp3.processing.checker.Checker;
 /**
  *
  * @author Tommy
  */
 public class Read {
-    private InputStream is;
+ 
+    private final static Logger LOGGER = Logger.getLogger(Checker.class.getName()); 
     
     /** Creates a new instance of Read */
     public Read() throws IOException {
         byte [] b = new byte [50000];
-        //String fileName = "/audio/440-128-CBR.mp3";     //2446
-        String fileName = "/audio/trombe.mp3";     //2446
-        //String fileName = "/audio/shine.mp3";         //0?
-        //String fileName = "/audio/vari.mp3";
-        is = getClass().getResourceAsStream(fileName);
+        
+        String fileName = System.getProperty("user.dir") + "/output/test.mp3";
+        File inputFile = new File(fileName);
+        InputStream is = new FileInputStream(inputFile);
         
         is.read(b,0,50000);
         
         for(int i = 0; i < b.length; i++)
         {
-            System.out.print("byte [" + i + "]: " + b[i]);
-            if((i % 418) == 25)  System.out.print(" <-- begin of frame " + (i / 418 + 1));
+        	StringBuffer sb = new StringBuffer();
+        	sb.append("byte [" + i + "]: " + b[i]);
+            if((i % 418) == 25)  sb.append(" <-- begin of frame " + (i / 418 + 1));
             
             if((i % 418) == 29){
                 System.out.print(" <-- main_data_beg: ");
                 int beg = (b[i] >= 0) ? ((b[i] << 1)+((b[i+1]>>> 7) & 1)): (((b[i] + 256) << 1)+((b[i+1]>>> 7) & 1));
-                System.out.print(beg);
+                sb.append(beg);
             }
             
-            if((i % 418) == 61)  System.out.print(" <-- begin of data in frame");
-            System.out.print("\n");       
+            if((i % 418) == 61)  sb.append(" <-- begin of data in frame");
+
+            LOGGER.info(sb.toString());
         }
+        
+        is.close();
     }
     
 }
